e25e84db0177451df3ab34d3df6cd2e0e09f5efb
Cornachione <leonardo.cornachione@dxc.com>
2019-03-15 19:47:58 -0300

    first commit

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/dicesdm/MainActivity.kt
@@ -0,0 +1,44 @@
+package br.edu.ifsp.scl.sdm.dicesdm
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    val geradorRandomico: Random = Random(System.currentTimeMillis())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun jogarDado(view: View) {
+        if (view == jogarDadoButton) {
+            val numDices: Int = numDicesSpinner.selectedItem.toString().toInt()
+            val numFaces = numFacesEditText.text.toString().toInt()
+            if (numFaces > 6) {
+                resultadoImageView.visibility = View.GONE
+                resultado2imageView.visibility = View.GONE
+            } else {
+                resultadoImageView.visibility = View.VISIBLE
+                if (numDices == 2) {
+                    resultado2imageView.visibility = View.VISIBLE
+                } else {
+                    resultado2imageView.visibility = View.GONE
+                }
+            }
+            var resultadoText = ""
+            for (i in 1..numDices) {
+                val resultado = geradorRandomico.nextInt(numFaces) + 1
+                resultadoText = "${resultadoText} $resultado"
+                val imageView: ImageView = if (i == 1) resultadoImageView else resultado2imageView
+                val resourceName = "dice_${resultado}"
+                imageView.setImageResource(resources.getIdentifier(resourceName, "drawable", packageName))
+                resultadoTextView.text = "A face sorteada foi a ${resultadoText}"
+            }
+        }
+    }
+}
